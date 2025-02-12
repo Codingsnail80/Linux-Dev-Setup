7d04899da117c04c6dc8a1cc16a0a2e6376a9b42
Codingsnail80 <andrewdavidshumake@gmail.com>
2025-02-12 16:39:55 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt update
+sudo apt install -y make cmake gcc g++ libc6-dev libpng-dev libjpeg-dev libxi-dev libgl1-mesa-dev libsqlite3-dev libvorbis-dev libopenal-dev libcurl4-gnutls-dev libfreetype6-dev zlib1g-dev libgmp-dev libjsoncpp-dev libzstd-dev libluajit-5.1-dev gettext git wget vim python3 cool-retro-term snapd sudo apt install default-jdk curl
+sudo snap install code --classic
+echo "Install has finished!"
